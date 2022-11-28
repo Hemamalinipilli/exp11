ecea3f1f6969514144c78d066029aa2da5b685bd
honey <honey@LAPTOP-R6451UU6>
2022-11-28 20:30:41 +0530

    first

---
+++ src/test/java/EXP11/exp11/AppTest.java
@@ -0,0 +1,23 @@
+package EXP11.exp11;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test()
+    {
+    	App a=new App();
+    	int actual=a.add(15,15);
+    	int expected=30;
+        assertEquals(expected,actual);
+    }
+}
